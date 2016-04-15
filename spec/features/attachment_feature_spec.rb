f3eac84f605767199773b3f9e03ade90e3931b73
eoris <eoris.work@gmail.com>
2016-04-15 16:56:25 +0300

    attach feature

--- spec/features/attachment_feature_spec.rb
+++ spec/features/attachment_feature_spec.rb
@@ -39,31 +39,4 @@ RSpec.feature "Attachment", js: true do
     expect(page).to have_content 'Comment'
     expect(page).not_to have_content 'lorem.txt'
   end
-
-  scenario "User delete not loaded attachment" do
-    project = create(:project, title: 'Hello Capybara', user_id: @user.id)
-    create(:task, title: 'Task 1', project_id: project.id)
-    expect(page).to have_content I18n.t('projects.add_todo_list')
-    expect(page).to have_content 'Hello Capybara'
-    expect(page).to have_content 'Task 1'
-
-    find("tbody > tr").hover
-    find(".task-comment").click
-    attach_file_script
-    expect(page).to have_content 'SIMPLE TODO LIST'
-    expect(page).to have_content 'FROM RUBY GARAGE'
-    expect(page).to have_content I18n.t('session.sign_out')
-    expect(page).to have_content I18n.t('comments.attach_files')
-    expect(page).to have_content I18n.t('comments.add_comment')
-    expect(page).to have_content I18n.t('comments.close')
-    expect(page).to have_css('.attachment')
-    expect(page).to have_css('.delete-upload-attachment')
-    fill_in 'comment-textarea', with: 'Comment'
-    expect(page).to have_content 'lorem.txt'
-    find(".delete-upload-attachment").click
-    click_button I18n.t('comments.add_comment')
-
-    expect(page).to have_content 'Comment'
-    expect(page).not_to have_content 'lorem.txt'
-  end
 end
