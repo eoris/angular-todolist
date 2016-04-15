791f6950c2ae2fb4f5cca671b6adc6f3848ad0af
eoris <eoris.work@gmail.com>
2016-04-15 08:13:30 +0300

    attachment feature test

--- spec/features/attachment_feature_spec.rb
+++ spec/features/attachment_feature_spec.rb
@@ -35,7 +35,26 @@ RSpec.describe "Attachment", type: :feature do
     fill_in 'comment-textarea', with: 'Comment'
     attach_file_script
     click_button I18n.t('comments.add_comment')
-    find(".times").click
+    find(".delete-attachment").click
+
+    expect(page).to have_content 'Comment'
+    expect(page).not_to have_content 'lorem.txt'
+  end
+
+  scenario "User delete not loaded attachment" do
+    project = create(:project, title: 'Hello Capybara', user_id: @user.id)
+    create(:task, title: 'Task 1', project_id: project.id)
+    file = create(:attachment)
+    expect(page).to have_content I18n.t('projects.add_todo_list')
+    expect(page).to have_content 'Hello Capybara'
+    expect(page).to have_content 'Task 1'
+
+    find("tbody > tr").hover
+    find(".task-comment").click
+    fill_in 'comment-textarea', with: 'Comment'
+    attach_file_script
+    click_button I18n.t('comments.add_comment')
+    find(".delete-upload-attachment").click
 
     expect(page).to have_content 'Comment'
     expect(page).not_to have_content 'lorem.txt'
