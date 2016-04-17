8c2cb56376e79ce50dd75709773eaf1b0e8255f7
eoris <eoris.work@gmail.com>
2016-04-17 16:53:48 +0300

    position

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,4 @@ class Task < ActiveRecord::Base
   belongs_to :project
   has_many  :comments, dependent: :destroy
   validates :title, :project_id, presence: true
-
-  default_scope { order('position ASC') }
-  default_scope { order('id ASC') }
 end
