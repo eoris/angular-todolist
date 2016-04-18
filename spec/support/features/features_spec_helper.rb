82784e289bbf990d7904bb1650e365ea15420d43
eoris <eoris.work@gmail.com>
2016-04-18 17:12:47 +0300

    #set_cookies

--- spec/support/features/features_spec_helper.rb
+++ spec/support/features/features_spec_helper.rb
@@ -1,7 +1,7 @@
 def sign_in_auth
   @user = create(:user)
   auth_headers = @user.create_new_auth_token
-  # Uncoment for selenium driver
+  # Uncomment for selenium driver
   # visit root_path
   page.driver.set_cookie('auth_headers', auth_headers.to_json)
 end
